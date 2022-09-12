5e21dff97f550d6fb6e4bc92d537c236e3c1a1b2
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2022-09-11 23:07:13 -0300

    js Login
    Seleccion de parametros del login

--- src/app/login.js
+++ src/app/login.js
@@ -7,9 +7,9 @@ const btnIngesar = document.getElementById('btnIngesar');
 formLogin.addEventListener('submit', (e) => {
   e.preventDefault();
   if (categoria.value === 'universidad') {
-    // location = './uni.html';
+    location = './layout_universidad/universidadInicio.html';
   } else if (categoria.value === 'ministerio') {
-    // alert('HOLA MINISTERIO');
+    alert('HOLA MINISTERIO');
   } else {
     location = './layout_alumno/alumnosInicio.html';
   }
